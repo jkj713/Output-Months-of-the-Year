b17a39c0f69a9706af583025026a81d5f281867d
jkj713 <jennifer.jimenez226@my.tccd.edu>
2020-03-04 19:42:20 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  //Declaration and Initialize Variables
+  string month1, month2, month3, month4, month5, month6, month7, month8, month9, month10, month11, month12;
+
+  month1 = "January";
+  month2 = "February";
+  month3 = "March";
+  month4 = "April";
+  month5 = "May";
+  month6 = "June";
+  month7 = "July";
+  month8 = "August";
+  month9 = "September";
+  month10 = "October";
+  month11 = "November";
+  month12 = "December";
+
+ //Output
+ cout << month1 << endl;
+ cout << month2 << endl;
+ cout << month3 << endl;
+ cout << month4 << endl;
+ cout << month5 << endl;
+ cout << month6 << endl;
+ cout << month7 << endl;
+ cout << month8 << endl;
+ cout << month9 << endl;
+ cout << month10 << endl;
+ cout << month11 << endl;
+ cout << month12 << endl;
+
+ return 0;
+}
